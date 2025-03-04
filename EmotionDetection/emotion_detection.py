1bab931e9b6286b0dfcadeae1e02ef3e98dd22b9
kodonou narcisse <narcissekodonou@gmail.com>
2025-03-04 03:25:05 -0500

    second commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,7 +6,17 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header) 
     formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    emotions["dominant_emotion"] = dominant_emotion
+    print(response.status_code)
+    if response.status_code == 500:
+        emotions['anger'] = None
+        emotions['disgust'] = None
+        emotions['fear'] = None
+        emotions['joy'] = None
+        emotions['sadness'] = None
+        emotions['dominant_emotion'] = None   
+    elif response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        emotions["dominant_emotion"] = dominant_emotion
+            
     return emotions
--- server.py
+++ server.py
@@ -4,9 +4,10 @@
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
-
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 #Initiate the flask app : TODO
-
+app = Flask('emotionDetector')
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
@@ -15,6 +16,13 @@ def sent_analyzer():
         score for the provided text.
     '''
     # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    
+    if response['dominant_emotion'] is None:
+        return "Invalid input! Try again."
+    else:
+        return "For given statment, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, 'sadness': {}, The dominant emotion is {}".format(response['anger'],response['disgust'],response['fear'],response['joy'],response['sadness'],response['dominant_emotion'])
 
 @app.route("/")
 def render_index_page():
@@ -22,7 +30,9 @@ def render_index_page():
         page over the Flask channel
     '''
     #TODO
+    return render_template("index.html")
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(host="0.0.0.0", port=5000)
